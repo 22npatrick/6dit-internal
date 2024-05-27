d654bc85e0c4235a7db06abfba59b47cbb0e1114
22npatrick <22npatrick@wakatipu.school.nz>
2024-05-28 09:51:28 +1200

    V2  - Added user inputs
    Added 2 queries

--- DITinternalPlanets.py
+++ DITinternalPlanets.py
@@ -1,8 +1,28 @@
 import sqlite3
 conn = sqlite3.connect('Planetdb')
 cursor = conn.cursor()
-cursor.execute('SELECT * FROM planets;')
-results = cursor.fetchall()
-print(results)
+
+def get_only_official_planet():
+    # Execute an SQL statement
+    query = "SELECT * FROM planets where type != 'Dwarf Planet';"
+    cursor.execute(query)
+    official_planets = cursor.fetchall()
+    for official_planet in official_planets:
+        print(f"{official_planet[1]} is a {official_planet[2]}.")
+
+def get_all_planets():
+    cursor.execute('SELECT * FROM planets;')
+    results = cursor.fetchall()
+    print(results)
+
+while True:
+    choice = input("\n1. Get official planets\n2. Get all data\n3. Exit\n> ")
+    if choice == "1":
+        get_only_official_planet()
+    elif choice == "2":
+        get_all_planets()
+    elif choice == "3":
+        break
 conn.commit()
 conn.close()
+
